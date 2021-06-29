463448562637ed55af21bfc69590d550887658c6
박경은 <ruddms936@naver.com>
2021-06-30 03:34:06 +0900

    add: c# 예제 추가

--- c-sharp/async-simple/Program.cs
+++ c-sharp/async-simple/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace async_simple
 {
   class Program
   {
-    static void Main(string[] args)
+    async static Task Main()
     {
-      Console.WriteLine("");
+      Console.WriteLine("비동기 작업 시작");
+
+      await Task.WhenAll(new[]
+      {
+        WorkDo(200, "1번째 비동기 작업"),
+        WorkDo(1000, "2번째 비동기 작업"),
+        WorkDo(10, "3번째 비동기 작업"),
+        WorkDo(500, "4번째 비동기 작업"),
+        WorkDo(600, "5번째 비동기 작업"),
+      });
+
+      Console.WriteLine("비동기 작업 완료! 프로그램을 종료합니다.");
+    }
+
+    public static async Task WorkDo(int sec, string content)
+    {
+      await Task.Delay(sec);
+      Console.WriteLine("\t" + content);
     }
   }
 }
