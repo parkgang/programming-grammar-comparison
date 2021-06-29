9aa51cf829731e2df0a6aadc3565dec5f39dc2cb
박경은 <ruddms936@naver.com>
2021-06-30 03:34:43 +0900

    edit: 동일하게 함수명 변경

--- go/async-simple/main.go
+++ go/async-simple/main.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func task(channel chan string, sec time.Duration, content string) {
+func WorkDo(channel chan string, sec time.Duration, content string) {
 	time.Sleep(sec * time.Millisecond)
 	channel <- content
 }
@@ -15,11 +15,11 @@ func main() {
 
 	fmt.Println("비동기 작업 시작")
 
-	go task(message, 200, "1번째 비동기 작업")
-	go task(message, 1000, "2번째 비동기 작업")
-	go task(message, 10, "3번째 비동기 작업")
-	go task(message, 500, "4번째 비동기 작업")
-	go task(message, 600, "5번째 비동기 작업")
+	go WorkDo(message, 200, "1번째 비동기 작업")
+	go WorkDo(message, 1000, "2번째 비동기 작업")
+	go WorkDo(message, 10, "3번째 비동기 작업")
+	go WorkDo(message, 500, "4번째 비동기 작업")
+	go WorkDo(message, 600, "5번째 비동기 작업")
 
 	fmt.Printf("\t%s\n", <-message)
 	fmt.Printf("\t%s\n", <-message)
--- js/async-simple/index.js
+++ js/async-simple/index.js
@@ -1,4 +1,4 @@
-async function task(sec, content) {
+async function WorkDo(sec, content) {
   await new Promise((r) => {
     setTimeout(() => {
       console.log("\t" + content);
@@ -10,11 +10,11 @@ async function task(sec, content) {
 console.log("비동기 작업 시작");
 
 Promise.all([
-  task(200, "1번째 비동기 작업"),
-  task(1000, "2번째 비동기 작업"),
-  task(10, "3번째 비동기 작업"),
-  task(500, "4번째 비동기 작업"),
-  task(600, "5번째 비동기 작업"),
+  WorkDo(200, "1번째 비동기 작업"),
+  WorkDo(1000, "2번째 비동기 작업"),
+  WorkDo(10, "3번째 비동기 작업"),
+  WorkDo(500, "4번째 비동기 작업"),
+  WorkDo(600, "5번째 비동기 작업"),
 ]).then(() => {
   console.log("비동기 작업 완료! 프로그램을 종료합니다.");
 });
